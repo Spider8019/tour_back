75278f0c133ccf89f256fc37c8f629f8eedbc1f1
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-01 16:52:24 +0530

    Asdfad

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.get("/placesinacity",async(req,res)=>{
         placeCity:"Delhi",
         placeImage:"https://i.pinimg.com/originals/4a/67/35/4a673520927a900a3b37a1ad233fe1df.jpg"
     }]
-    res.send({status:200,result:arr})
+    const result = arr.map(({ placeName, placeImage }) => ({ placeName, placeImage }));
+    res.send({status:200,result})
 })
 app.listen(3000,()=>console.log("server is listening on port 3000"))
