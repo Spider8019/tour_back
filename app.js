5b1eeae3bd3ec0e43bc6256589bad480f04fdfb1
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-01 16:13:48 +0530

    asdf

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app=express()
 
 app.get("/placesinacity",async(req,res)=>{
     const arr=[{
-        placeName:"Shri Ram Janm Bhoomi",
-        placeImage:"https://www.thetoptours.com/wp-content/uploads/ram-janmabhoomi-1.jpg"
+        placeName:"Sree Ram Janmabhoomi Mandir",
+        placeImage:"https://i.pinimg.com/originals/39/aa/eb/39aaebc33872252aed06ee1af42fc79b.jpg"
+    },{
+        placeName:"Kanak Bhawan",
+        placeImage:"https://i.pinimg.com/originals/ca/75/2f/ca752fc70b74b2f2b7f8692e708ab737.jpg"
+    },{
+        placeName:"Ayodhya Junction",
+        placeImage:"https://images.indianexpress.com/2020/08/ayodhya-railways-station.jpg"
     }]
     res.send({status:200,result:arr})
 })
