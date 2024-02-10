52eacab3d508efc2467db85e4b8b80ddf3062364
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-10 23:08:48 +0530

    Adfa

--- arr.js
+++ arr.js
@@ -7,7 +7,16 @@ const arr = [
         'https://i.pinimg.com/originals/39/aa/eb/39aaebc33872252aed06ee1af42fc79b.jpg',
       placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
       moreImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
-      entryFees:0
+      entryFees:{
+        indian:{
+          child:0,
+          adult:0
+        },
+        foreigner:{
+          child:0,
+          adult:0
+        }
+      }
     },
     {
       placeId:2,
@@ -17,7 +26,16 @@ const arr = [
         'https://images.pexels.com/photos/2415399/pexels-photo-2415399.jpeg?auto=compress&cs=tinysrgb&w=800',
       placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
       moreImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
-      entryFees:35
+      entryFees:{
+        indian:{
+          child:0,
+          adult:35
+        },
+        foreigner:{
+          child:120,
+          adult:250
+        }
+      }
     },
     {
       placeId:3,
@@ -27,7 +45,16 @@ const arr = [
         'https://images.pexels.com/photos/13084084/pexels-photo-13084084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
       moreImages:['https://images.pexels.com/photos/18442273/pexels-photo-18442273/free-photo-of-husainabad-clock-tower-in-lucknow-in-india.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2','https://images.pexels.com/photos/16630819/pexels-photo-16630819/free-photo-of-husainabad-clock-tower-in-lucknow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'],
-      entryFees:35
+      entryFees:{
+        indian:{
+          child:0,
+          adult:0
+        },
+        foreigner:{
+          child:0,
+          adult:0
+        }
+      }
     },
     
   ]
