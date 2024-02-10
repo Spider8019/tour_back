bb6940ae0b15c3231fc55788439d2f4ca3b840bc
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-10 19:28:46 +0530

    Ad

--- arr.js
+++ arr.js
@@ -1,11 +1,12 @@
 const arr = [
     {
+      placeId:1,
       placeName: 'Sree Ram Janmabhoomi Mandir',
       placeCity: 'Ayodhya',
       placeImage:
         'https://i.pinimg.com/originals/39/aa/eb/39aaebc33872252aed06ee1af42fc79b.jpg',
       placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
-      carouselImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
+      moreImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
       entryFees:0
     },
     
