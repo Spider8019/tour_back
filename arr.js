009572756818cf1cee5c8ccb473272165e7657e4
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-11 23:19:10 +0530

    Adfa

--- arr.js
+++ arr.js
@@ -11,8 +11,12 @@ const arr = [
       text:
         'Husainabad Clock Tower Lucknow Address: Hussainabad Road, Husainabad, Lucknow, Uttar Pradesh, 226003, India',
     },
-    placeContent:
-      '<div><strong>Brief History</strong><p>The Hussainabad neighbourhood of Lucknow is home to the famed Hussainabad Clock Tower. To commemorate the arrival of Sir George Couper, the lieutenant governor of the North-West Province, Nawab Nasiruddiin Haider (1827&ndash;1837) built it. The construction of it cost 1.75 lakhs of rupees.It can be found close to Teele Wali Masjid, Bara Imambara, and Rumi Darwaza. Constructed in 1881, the Husainabad Clock Tower holds the title of highest clock tower in India.</p></div><div><strong>Architectural&nbsp;</strong><p>This 67-meter (220-foot) structure was designed by Richard Roskell Bayne and features Gothic and Victorian-style structural elements. The components of the clock are made of gunmetal. The massive 14-foot pendulum of this clock is accompanied by a 12-fully gold flower-shaped dial with bells ringing around it.</p></div>',
+    placeContent: {
+      content1:
+        '<div><strong>Brief History</strong><p>The Hussainabad neighbourhood of Lucknow is home to the famed Hussainabad Clock Tower. To commemorate the arrival of Sir George Couper, the lieutenant governor of the North-West Province, Nawab Nasiruddiin Haider (1827&ndash;1837) built it. The construction of it cost 1.75 lakhs of rupees.It can be found close to Teele Wali Masjid, Bara Imambara, and Rumi Darwaza. Constructed in 1881, the Husainabad Clock Tower holds the title of highest clock tower in India.</p></div>',
+      content2:
+        '<div><strong>Architectural&nbsp;</strong><p>This 67-meter (220-foot) structure was designed by Richard Roskell Bayne and features Gothic and Victorian-style structural elements. The components of the clock are made of gunmetal. The massive 14-foot pendulum of this clock is accompanied by a 12-fully gold flower-shaped dial with bells ringing around it.</p></div>',
+    },
     moreImages: [
       'https://images.pexels.com/photos/18442273/pexels-photo-18442273/free-photo-of-husainabad-clock-tower-in-lucknow-in-india.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       'https://images.pexels.com/photos/16630819/pexels-photo-16630819/free-photo-of-husainabad-clock-tower-in-lucknow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
