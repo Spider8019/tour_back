b3bd5a48a52b2396a1d1c0892d12c95e59b2fd0b
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-11 09:39:14 +0530

    adfa

--- arr.js
+++ arr.js
@@ -1,63 +1,32 @@
 const arr = [
-    {
-      placeId:1,
-      placeName: 'Sree Ram Janmabhoomi Mandir',
-      placeCity: 'Ayodhya',
-      placeImage:
-        'https://i.pinimg.com/originals/39/aa/eb/39aaebc33872252aed06ee1af42fc79b.jpg',
-      placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
-      moreImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
-      entryFees:{
-        indian:{
-          child:0,
-          adult:0
-        },
-        foreigner:{
-          child:0,
-          adult:0
-        }
-      }
+  {
+    placeId: 2,
+    placeName: 'Husainabad Clock Tower',
+    placeCity: 'Lucknow',
+    placeImage:
+      'https://images.pexels.com/photos/13084084/pexels-photo-13084084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    placeLocation: {
+      latitude: 26.874032316871155,
+      longitude: 80.90803425112806,
+      text:
+        'Husainabad Clock Tower Lucknow Address: Hussainabad Road, Husainabad, Lucknow, Uttar Pradesh, 226003, India',
     },
-    {
-      placeId:2,
-      placeName: 'Humayun Tomb',
-      placeCity: 'Delhi',
-      placeImage:
-        'https://images.pexels.com/photos/2415399/pexels-photo-2415399.jpeg?auto=compress&cs=tinysrgb&w=800',
-      placeContent:"Ayodhya, town in northern India that is known as a sacred city and the birthplace of Rama in Hinduism and as a historical center of Buddhism. On a site significant to both Hindus and Muslims was a Mughal-era mosque, the Babri Masjid, which was destroyed in 1992 amid interreligious tensions. After a 2019 Supreme Court verdict granting the site to Hindus, a new Ram temple is under construction, with an inauguration date of January 22, 2024. Ayodhya lies on the Ghaghara River, also known as Sarayu or Sarju in the lower course, just east of Faizabad in south-central Uttar Pradesh state. An ancient town, Ayodhya is regarded as one of the seven sacred cities of the Hindus, revered because of its association in the great Indian epic poem Ramayana with the birth of Rama and with the rule of his father, Dasharatha. According to this source, the town was prosperous and well fortified and had a large population.",
-      moreImages:['https://i.pinimg.com/originals/b0/9b/b8/b09bb805a1a96e8586edb0a6109a31b9.jpg','https://i.pinimg.com/736x/79/ab/2f/79ab2f9a570a971ea0b75b6d79158202.jpg'],
-      entryFees:{
-        indian:{
-          child:0,
-          adult:35
-        },
-        foreigner:{
-          child:120,
-          adult:250
-        }
-      }
+    placeContent:"<strong>Brief History</strong><p>The Hussainabad neighbourhood of Lucknow is home to the famed Hussainabad Clock Tower. To commemorate the arrival of Sir George Couper, the lieutenant governor of the North-West Province, Nawab Nasiruddiin Haider (1827&ndash;1837) built it. The construction of it cost 1.75 lakhs of rupees.It can be found close to Teele Wali Masjid, Bara Imambara, and Rumi Darwaza. Constructed in 1881, the Husainabad Clock Tower holds the title of highest clock tower in India.</p><strong>Architectural&nbsp;</strong><p>This 67-meter (220-foot) structure was designed by Richard Roskell Bayne and features Gothic and Victorian-style structural elements. The components of the clock are made of gunmetal. The massive 14-foot pendulum of this clock is accompanied by a 12-fully gold flower-shaped dial with bells ringing around it.</p>",
+    moreImages: [
+      'https://images.pexels.com/photos/18442273/pexels-photo-18442273/free-photo-of-husainabad-clock-tower-in-lucknow-in-india.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      'https://images.pexels.com/photos/16630819/pexels-photo-16630819/free-photo-of-husainabad-clock-tower-in-lucknow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    ],
+    entryFees: {
+      indian: {
+        child: 0,
+        adult: 0,
+      },
+      foreigner: {
+        child: 0,
+        adult: 0,
+      },
     },
-    {
-      placeId:3,
-      placeName: 'Husainabad Clock Tower',
-      placeCity: 'Lucknow',
-      placeImage:
-        'https://images.pexels.com/photos/13084084/pexels-photo-13084084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      placeExactLocation:"Husainabad Clock Tower Lucknow Address: Hussainabad Road, Husainabad, Lucknow, Uttar Pradesh, 226003, India",
-      placeContent:"The Hussainabad neighbourhood of Lucknow is home to the historic Hussainabad Clock Tower. After Sir George Cooper, the first lieutenant governor of the United Provinces of Oudh, arrived, the Hussainabad Trust constructed it.\n\n The highest clock tower in India, also known as Ghantaghar, is one of the most popular tourist spots in Lucknow.",
-      moreImages:['https://images.pexels.com/photos/18442273/pexels-photo-18442273/free-photo-of-husainabad-clock-tower-in-lucknow-in-india.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2','https://images.pexels.com/photos/16630819/pexels-photo-16630819/free-photo-of-husainabad-clock-tower-in-lucknow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'],
-      entryFees:{
-        indian:{
-          child:0,
-          adult:0
-        },
-        foreigner:{
-          child:0,
-          adult:0
-        }
-      }
-    },
-    
-  ]
+  },
+]
 
-  module.exports=arr
+module.exports = arr
