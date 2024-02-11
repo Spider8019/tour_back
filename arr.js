1662622940cfdfc0132644add559e996c0b99685
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-11 18:10:59 +0530

    ADfaD

--- arr.js
+++ arr.js
@@ -1,6 +1,6 @@
 const arr = [
   {
-    placeId: 2,
+    placeId: 1,
     placeName: 'Husainabad Clock Tower',
     placeCity: 'Lucknow',
     placeImage:
@@ -11,7 +11,8 @@ const arr = [
       text:
         'Husainabad Clock Tower Lucknow Address: Hussainabad Road, Husainabad, Lucknow, Uttar Pradesh, 226003, India',
     },
-    placeContent:"<div><strong>Brief History</strong><p>The Hussainabad neighbourhood of Lucknow is home to the famed Hussainabad Clock Tower. To commemorate the arrival of Sir George Couper, the lieutenant governor of the North-West Province, Nawab Nasiruddiin Haider (1827&ndash;1837) built it. The construction of it cost 1.75 lakhs of rupees.It can be found close to Teele Wali Masjid, Bara Imambara, and Rumi Darwaza. Constructed in 1881, the Husainabad Clock Tower holds the title of highest clock tower in India.</p></div><div><strong>Architectural&nbsp;</strong><p>This 67-meter (220-foot) structure was designed by Richard Roskell Bayne and features Gothic and Victorian-style structural elements. The components of the clock are made of gunmetal. The massive 14-foot pendulum of this clock is accompanied by a 12-fully gold flower-shaped dial with bells ringing around it.</p></div>",
+    placeContent:
+      '<div><strong>Brief History</strong><p>The Hussainabad neighbourhood of Lucknow is home to the famed Hussainabad Clock Tower. To commemorate the arrival of Sir George Couper, the lieutenant governor of the North-West Province, Nawab Nasiruddiin Haider (1827&ndash;1837) built it. The construction of it cost 1.75 lakhs of rupees.It can be found close to Teele Wali Masjid, Bara Imambara, and Rumi Darwaza. Constructed in 1881, the Husainabad Clock Tower holds the title of highest clock tower in India.</p></div><div><strong>Architectural&nbsp;</strong><p>This 67-meter (220-foot) structure was designed by Richard Roskell Bayne and features Gothic and Victorian-style structural elements. The components of the clock are made of gunmetal. The massive 14-foot pendulum of this clock is accompanied by a 12-fully gold flower-shaped dial with bells ringing around it.</p></div>',
     moreImages: [
       'https://images.pexels.com/photos/18442273/pexels-photo-18442273/free-photo-of-husainabad-clock-tower-in-lucknow-in-india.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       'https://images.pexels.com/photos/16630819/pexels-photo-16630819/free-photo-of-husainabad-clock-tower-in-lucknow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -24,6 +25,37 @@ const arr = [
       foreigner: {
         child: 0,
         adult: 0,
+      },
+    },
+  },
+  {
+    placeId: 2,
+    placeName: 'Bara Imambara',
+    placeCity: 'Lucknow',
+    placeImage:
+      'https://i.pinimg.com/564x/e0/72/c3/e072c385f25f076f840179609a5c2df3.jpg',
+    placeLocation: {
+      latitude: 26.869512328450885,
+      longitude: 80.91260842414347,
+      text:
+        'Near Machchhi Bhavan, Machchhi Bhavan, Lucknow, Uttar Pradesh, 226003, India',
+    },
+    placeContent:
+      '<div><strong>Brief History</strong><p>The construction of this magnificent structure was started in the year 1784 by Nawab Asaf-Ud-Daula, the fourth Nawab of Awadh. It took more than 14 years to finish. Bara Imambara\'s remarkable history captivates the attention of all tourists visiting this fascinating location. The masterpiece results from Nawab\'s love and devotion for his people. When the Awadh region was severely affected by a disaster, increasing employment was the purpose of erecting this monument. A catastrophic famine that hit the Awadh Estate in the 18th century prompted the Nawab to devise a strategy to feed his starving subjects. He decided to require them to build great structures, which would then give them work and, consequently, food. #Trivia: More than 20,000 people—rich, poor, and elite—were employed by Asaf-ud-relief Daula\'s effort, which took 11 years to complete—exactly how long the famine lasted. The working class and poor started the project in the morning, and all of the construction was destroyed in the evening by the upper class. Every one was guaranteed to work and support themselves until conditions returned to normal in 1791.</p></div><div><strong>Architectural&nbsp;</strong><p>The Bara Imambara was constructed using Gothic, Rajput, and Mughal architectural elements. It was created by one of the most respected architects, Hafiz Kifayat Ullah. \n There are 9 halls inside the monument, with the largest being the central hall. Trivia: It is one of the largest arched structures in the world, measuring 50 X 16 meters and standing over 15 meters tall with no beams supporting the ceiling. Its construction is distinctive because it contains no woodwork other than what is found in the galleries. The roof is supported only by the interlocking bricks used to assemble the blocks and stands erect without any pillars.The Imamabara is made of Lakhauri bricks and is decorated with stucco work, chattris, and parapets. This Imambara is distinctive because its roof is constructed with rice husk ash. Bara Imambara is locally famous for its amazing maze, also called Bhool Bhulaiya. The topmost floor of the monument\'s structure is filled with an alluring maze of little tunnels that eventually leads to the rooftop.There are 1024 ways to enter the maze, but only two methods to exit it. One may have a comprehensive perspective of the city from the roof.<p></div>',
+    moreImages: [
+      'https://i.pinimg.com/564x/e4/c0/89/e4c0895ca16033f4b599f75bf814d0c3.jpg',
+      'https://i.pinimg.com/564x/ec/4d/2d/ec4d2d7235582d50a4e2eb167b3d6563.jpg',
+      'https://i.pinimg.com/564x/1e/8a/98/1e8a98f75c5994ffed7614d47b6297e8.jpg',
+      'https://i.pinimg.com/564x/d0/74/65/d07465fe1dbf0f82d6c878e438a26e84.jpg'
+    ],
+    entryFees: {
+      indian: {
+        child: 50,
+        adult: 25,
+      },
+      foreigner: {
+        child: 500,
+        adult: 500,
       },
     },
   },
