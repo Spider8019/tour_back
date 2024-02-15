b71f6d9654deb1fe039349139c6e87a7656cc7ad
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-15 18:50:43 +0530

    Adaf

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ const cors = require('cors')
 
 var placesInACityRouter = require('./routes/placesInACity')
 
+mongooseConn.once('open', function () {
+  console.log('MongoDB database connection established successfully')
+})
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+
 app.use('/placesinacity', placesInACityRouter)
 
+app.get("/",async(req,res)=>{
+  res.send("Hello world")
+})
 app.get('/placefromacity', async (req, res) => {
   const result = arr.filter((item) => item.placeName == req.query.placeName)
   res.send(result[0])
 })
 
-mongooseConn.once('open', function () {
-  console.log('MongoDB database connection established successfully')
-})
 app.listen(3000, () => console.log('Server is listening on port 3000'))
 
 // Error handling middleware
--- connections/mongoose.js
+++ connections/mongoose.js
@@ -1,13 +1,4 @@
-//
 
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb+srv://spider8019official:spider8019official@cluster0.ptqucsb.mongodb.net/";
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
 
 var mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://spider8019official:spider8019official@cluster0.ptqucsb.mongodb.net/nodeapp');
