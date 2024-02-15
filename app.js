9706f2204e0d4734bf2b3c2a2b61fc48940dd362
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-15 18:34:54 +0530

    adfka;sldkf

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-const express = require('express')
-const arr = require('./arr')
-const app = express()
-const mongooseConn = require('./connections/mongoose')
-const cors = require('cors')
-// const pool=require("./connections/mysql")
+const express = require('express');
+const arr = require('./arr');
+const app = express();
+const mongooseConn = require('./connections/mongoose');
+const cors = require('cors');
 
-var placesInACityRouter = require('./routes/placesInACity')
+var placesInACityRouter = require('./routes/placesInACity');
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/placesinacity', placesInACityRouter)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/placesinacity', placesInACityRouter);
 
 app.get('/placefromacity', async (req, res) => {
-  // res.send(arr)
-  const result = arr.filter((item) => item.placeName == req.query.placeName)
-  // res.send(result)
-  res.send(result[0])
-})
+    const result = arr.filter((item) => item.placeName == req.query.placeName);
+    res.send(result[0]);
+});
 
 mongooseConn.once('open', function () {
-  console.log('MongoDB database connection established successfully')
-  app.listen(3000, () => console.log('Server is listening on port 3000'))
-})
+    console.log('MongoDB database connection established successfully');
+    app.listen(3000, () => console.log('Server is listening on port 3000'));
+});
 
-mongooseConn.on('error', function (err) {
-  console.error('MongoDB connection error:', err)
-  res.status(500).send('Database connection error') // Return a 500 status code with an error message
-})
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error('MongoDB connection error:', err);
+    res.status(500).send('Database connection error');
+});
 
-// app.get('/placesinacity', async (req, res) => {
-//   const result = arr
-//     .filter((item) => item.placeCity == req.query.cityName)
-//     .map(({ placeName, placeImage }) => ({ placeName, placeImage }))
-//   res.send(result )
-// })
