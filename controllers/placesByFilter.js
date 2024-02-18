1a830b82ff39429913127987c7040d9d07fa74c7
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-18 21:42:07 +0530

    Extending city anme

--- controllers/placesByFilter.js
+++ controllers/placesByFilter.js
@@ -1,14 +1,16 @@
 const Place = require('../models/attractions')
 
 exports.getPlacesByFilter = (req, res) => { // Assuming categoryName is passed as a query parameter
-    Place.getPlacesByProvidedFilter(req.query)
-      .then((data) => {
-        const result = data
-          .map(({ placeName, placeImage }) => ({ placeName, placeImage }))
-        res.send(result)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Internal Server Error' + err)
-      })
-  }
+  Place.getPlacesByProvidedFilter(req.query)
+    .then((data) => {
+      const result = data
+        .map(({ placeName, placeImage, placeCity }) => ({
+          placeName, placeImage, placeCity
+        }))
+      res.send(result)
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send('Internal Server Error' + err)
+    })
+}
